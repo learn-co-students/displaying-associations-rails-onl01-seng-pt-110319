5b715d7dbce2d8fb306e02868ab6155e4e7d0677
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-04-19 04:56:51 +0000

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 clickbait = Category.create!(name: "Motivation")
 clickbait.posts.create!(title: "10 Ways You Are Awesome")
-clickbait.posts.create!(title:) "This Yoga Stretch Cures Procrastination, Maybe")
-clickbait.posts.create!(title:) "The Power of Positive Thinking and 100 Gallons of Coffee")
+clickbait.posts.create!(title: "This Yoga Stretch Cures Procrastination, Maybe")
+clickbait.posts.create!(title: "The Power of Positive Thinking and 100 Gallons of Coffee")
 
 movies = Category.create!(name: "Movies")
 movies.posts.create!(title: "Top 20 Summer Blockbusters Featuring a Cute Dog")
